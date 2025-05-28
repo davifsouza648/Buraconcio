56120fd9a989583c950c65347eb63519ad965ded
Arthur Alves Ribeiro Gordo <arthurarg100@gmail.com>
2025-05-28 10:33:18 -0300

    implemented shooting the ball

---
+++ core/src/main/java/io/github/buraconcio/Utils/Constants.java
@@ -0,0 +1,7 @@
+package io.github.buraconcio.Utils;
+
+public class Constants {
+    public static final float BALL_RADIUS = 0.5f;
+    public static final float MAX_IMPULSE = 10f;
+    public static final float MAX_IMPULSE_DISTANCE = 3f; // quanto puxar o mouse pra ter forca maxima
+}
