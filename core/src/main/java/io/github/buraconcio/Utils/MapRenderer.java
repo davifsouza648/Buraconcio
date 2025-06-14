8d3b49f0645a3a4a15af4abd2c2a43c9a4f28645
Mário <mmm.masaomukai@gmail.com>
2025-06-14 09:14:13 -0300

    Change in curve collision logic

--- core/src/main/java/io/github/buraconcio/Utils/MapRenderer.java
+++ core/src/main/java/io/github/buraconcio/Utils/MapRenderer.java
@@ -2,18 +2,15 @@ package io.github.buraconcio.Utils;
 
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.objects.EllipseMapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Ellipse;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 
 import io.github.buraconcio.Objects.PhysicsEntity;
 import io.github.buraconcio.Objects.Flag;
@@ -31,16 +28,14 @@ public class MapRenderer extends OrthogonalTiledMapRenderer
     public void createCollisions()
     {
         float pixelsPerMeter = 32f;
-        float tileSize = 1f;
 
         MapObjects objects = getMap().getLayers().get("Objetos").getObjects();
 
-        for(MapObject object: objects)
+        for (MapObject object : objects)
         {
-            // System.out.println(object.getName());
-            if ("Box".equals(object.getName())) //No tiled apenas sete o nome do objeto criado que ele será reconhecido aqui, caso não esteja nesses ifs, crie um.
+            if ("Box".equals(object.getName()))
             {
-                Rectangle rect = ((RectangleMapObject)object).getRectangle();
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
 
                 float x = (rect.x + rect.width / 2) / pixelsPerMeter;
                 float y = (rect.y + rect.height / 2) / pixelsPerMeter;
@@ -54,131 +49,107 @@ public class MapRenderer extends OrthogonalTiledMapRenderer
                 shape.dispose();
             }
 
-            else if("Buraco".equals(object.getName()))
+            else if ("Buraco".equals(object.getName()))
             {
-                Rectangle rect = ((RectangleMapObject)object).getRectangle();
-                Vector2 pos = new Vector2((rect.x + rect.width / 2f) / pixelsPerMeter, (rect.y + rect.height/2f) / pixelsPerMeter);
+                Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                Vector2 pos = new Vector2((rect.x + rect.width / 2f) / pixelsPerMeter, (rect.y + rect.height / 2f) / pixelsPerMeter);
                 new Flag(pos, rect.width / pixelsPerMeter);
             }
+
             else if (object.getName() != null && object.getName().startsWith("Curva"))
             {
                 Ellipse ellipse = ((EllipseMapObject) object).getEllipse();
-
-                float x = ellipse.x / pixelsPerMeter;
-                float y = ellipse.y / pixelsPerMeter;
-                float width = ellipse.width / pixelsPerMeter;
-                float height = ellipse.height / pixelsPerMeter;
-
-                int curvaIndex = 1;
-                try {
-                    String name = object.getName().replaceAll("[^0-9]", "");
-                    if (!name.isEmpty()) curvaIndex = Integer.parseInt(name);
-                } catch (NumberFormatException ignored) {}
-
-                float centerX = x;
-                float centerY = y;
-                float startAngle = 0;
-
-                switch (curvaIndex) {
-                    case 1: // inferior esquerdo
-                        centerX = x + width - 8f/pixelsPerMeter;
-                        centerY = y + height - 8f/pixelsPerMeter;
-                        startAngle = (float) Math.PI;
-                        break;
-                    case 2: // inferior direito
-                        centerX = x + 8f/pixelsPerMeter;
-                        centerY = y + height - 8f/pixelsPerMeter;
-                        startAngle = (float) (3 * Math.PI / 2);
-                        break;
-                    case 3: // superior direito
-                        centerX = x + 8f/pixelsPerMeter;
-                        centerY = y + 8f/pixelsPerMeter;
-                        startAngle = 0f;
-                        break;
-                    case 4: // superior esquerdo
-                        centerX = x + width - 8f/pixelsPerMeter;
-                        centerY = y + 8f/pixelsPerMeter;
-                        startAngle = (float) (Math.PI / 2);
-                        break;
-                }
-
-                float outerRadius = width; 
-                float thickness = 0.2f;
-                float innerRadius = outerRadius - thickness;
-
-                int segments = 10;
-                Vector2[] outerArc = new Vector2[segments + 1];
-                Vector2[] innerArc = new Vector2[segments + 1];
-
-                for (int i = 0; i <= segments; i++) {
-                    float angle = startAngle + (float) (Math.PI / 2) * i / segments;
-
-                    outerArc[i] = new Vector2(
-                        centerX + (float) Math.cos(angle) * outerRadius,
-                        centerY + (float) Math.sin(angle) * outerRadius
-                    );
-                    innerArc[i] = new Vector2(
-                        centerX + (float) Math.cos(angle) * innerRadius,
-                        centerY + (float) Math.sin(angle) * innerRadius
-                    );
-                }
-
-                PhysicsEntity entity = new PhysicsEntity(new Vector2(centerX, centerY), new Vector2(outerRadius * 2, outerRadius * 2));
-
-                for (int i = 0; i < segments; i++) {
-                    Vector2 p1 = innerArc[i];
-                    Vector2 p2 = innerArc[i + 1];
-                    Vector2 p3 = outerArc[i + 1];
-                    Vector2 p4 = outerArc[i];
-
-                    // Triângulo 1
-                    PolygonShape shape1 = new PolygonShape();
-                    shape1.set(new Vector2[]{
-                        new Vector2(p1.x - centerX, p1.y - centerY),
-                        new Vector2(p2.x - centerX, p2.y - centerY),
-                        new Vector2(p3.x - centerX, p3.y - centerY)
-                    });
-                    entity.getBody().createFixture(shape1, 0f);
-                    shape1.dispose();
-
-                    // Triângulo 2
-                    PolygonShape shape2 = new PolygonShape();
-                    shape2.set(new Vector2[]{
-                        new Vector2(p1.x - centerX, p1.y - centerY),
-                        new Vector2(p3.x - centerX, p3.y - centerY),
-                        new Vector2(p4.x - centerX, p4.y - centerY)
-                    });
-                    entity.getBody().createFixture(shape2, 0f);
-                    shape2.dispose();
-                }
+                createArcCollider(ellipse, object.getName(), pixelsPerMeter, 0.005f);
             }
-
-
         }
-
-        // for (int x = 0; x < layer.getWidth(); x++)
-        // {
-        //     for (int y = 0; y < layer.getHeight(); y++)
-        //     {
-        //         TiledMapTileLayer.Cell cell = layer.getCell(x, y);
-        //         if (cell != null && cell.getTile() != null)
-        //         {
-        //             int tileId = cell.getTile().getId();
-
-        //             if (tileId == 3)
-        //             {
-        //                 PhysicsEntity wall1 = new PhysicsEntity(new Vector2((x + 0.5f) * tileSize, (y + 0.5f) * tileSize), new Vector2(tileSize, tileSize));
-        //                 PolygonShape wallBox = new PolygonShape();
-        //                 wallBox.setAsBox(tileSize / 2, tileSize / 2);
-        //                 wall1.getBody().createFixture(wallBox, 0f);
-        //                 wallBox.dispose();
-        //             }
-        //         }
-        //     }
-        // }
     }
 
+    private void createArcCollider(Ellipse ellipse, String name, float pixelsPerMeter, float thickness)
+    {
+        float x = ellipse.x / pixelsPerMeter;
+        float y = ellipse.y / pixelsPerMeter;
+        float width = ellipse.width / pixelsPerMeter;
+        float height = ellipse.height / pixelsPerMeter;
 
+        float centerX = x + width / 2f;
+        float centerY = y + height / 2f;
 
+        int curvaIndex = 1;
+        try {
+            String indexString = name.replaceAll("[^0-9]", "");
+            if (!indexString.isEmpty()) curvaIndex = Integer.parseInt(indexString);
+        } catch (NumberFormatException ignored) {}
 
+        float startAngle = 0f;
+
+        switch (curvaIndex) {
+            case 1: // inferior esquerdo
+                startAngle = (float) Math.PI;
+                break;
+            case 2: // inferior direito
+                startAngle = (float) (3 * Math.PI / 2);
+                break;
+            case 3: // superior direito
+                startAngle = 0f;
+                break;
+            case 4: // superior esquerdo
+                startAngle = (float) (Math.PI / 2);
+                break;
+        }
+
+        // Raios da elipse
+        float outerRadiusX = width / 2f;
+        float outerRadiusY = height / 2f;
+        float innerRadiusX = outerRadiusX - thickness;
+        float innerRadiusY = outerRadiusY - thickness;
+
+        // Suavidade da curva (aumente para mais suave)
+        int segments = 10;
+
+        Vector2[] outerArc = new Vector2[segments + 1];
+        Vector2[] innerArc = new Vector2[segments + 1];
+
+        for (int i = 0; i <= segments; i++) {
+            float angle = startAngle + (float) (Math.PI / 2) * i / segments;
+
+            outerArc[i] = new Vector2(
+                centerX + (float) Math.cos(angle) * outerRadiusX,
+                centerY + (float) Math.sin(angle) * outerRadiusY
+            );
+            innerArc[i] = new Vector2(
+                centerX + (float) Math.cos(angle) * innerRadiusX,
+                centerY + (float) Math.sin(angle) * innerRadiusY
+            );
+        }
+
+        // Cria entidade física com corpo para a curva
+        PhysicsEntity entity = new PhysicsEntity(new Vector2(centerX, centerY), new Vector2(width, height));
+
+        for (int i = 0; i < segments; i++) {
+            Vector2 p1 = innerArc[i];
+            Vector2 p2 = innerArc[i + 1];
+            Vector2 p3 = outerArc[i + 1];
+            Vector2 p4 = outerArc[i];
+
+            // Triângulo 1
+            PolygonShape shape1 = new PolygonShape();
+            shape1.set(new Vector2[]{
+                new Vector2(p1.x - centerX, p1.y - centerY),
+                new Vector2(p2.x - centerX, p2.y - centerY),
+                new Vector2(p3.x - centerX, p3.y - centerY)
+            });
+            entity.getBody().createFixture(shape1, 0f);
+            shape1.dispose();
+
+            // Triângulo 2
+            PolygonShape shape2 = new PolygonShape();
+            shape2.set(new Vector2[]{
+                new Vector2(p1.x - centerX, p1.y - centerY),
+                new Vector2(p3.x - centerX, p3.y - centerY),
+                new Vector2(p4.x - centerX, p4.y - centerY)
+            });
+            entity.getBody().createFixture(shape2, 0f);
+            shape2.dispose();
+        }
+    }
 }
